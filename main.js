1701adf4ed1508cb9753bce090aba85138ad11cc
Febry Billiyagi <89958256+billiyagi@users.noreply.github.com>
2023-12-15 05:47:49 +0700

    rewrite main.js

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 const { persiapan, rebusAir, masak, download } = require('./modul')
 
-const main = () => {
-    persiapan().then((result) => {
-        console.log(result);
-        return rebusAir();
-    }).then((result) => {
-        console.log(result);
-        return masak();
-    }).then((result) => {
-        console.log(result)
-        return download()
-    });
+const main = async () => {
+    console.log(await persiapan());
+    console.log(await rebusAir());
+    console.log(await masak());
+    console.log(await download());
 }
 
 main();
